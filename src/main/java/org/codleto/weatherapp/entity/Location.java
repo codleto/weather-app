132ed8fe6768114da0dfc3020c5cb212bdff35b4
codleto <codleto@gmail.com>
2026-05-28 11:53:49 +0300

    feat: add Location.java

--- src/main/java/org/codleto/weatherapp/entity/Location.java
+++ src/main/java/org/codleto/weatherapp/entity/Location.java
@@ -1,4 +1,30 @@
 package org.codleto.weatherapp.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "locations")
+@NoArgsConstructor
+@Getter
 public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private BigDecimal latitude;
+
+    private BigDecimal longitude;
+
 }
