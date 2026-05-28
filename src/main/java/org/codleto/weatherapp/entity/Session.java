cb59d65f774d48bb21a9f6677738b8d18d363db5
codleto <codleto@gmail.com>
2026-05-28 11:52:50 +0300

    feat: add Session entity for database mapping

---
+++ src/main/java/org/codleto/weatherapp/entity/Session.java
@@ -0,0 +1,31 @@
+package org.codleto.weatherapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "sessions")
+@Getter
+@NoArgsConstructor
+public class Session {
+
+    @Id
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @JoinColumn(name = "expires_at", nullable = false)
+    private LocalDateTime ExpiresAt;
+
+    public Session(UUID id, User user, LocalDateTime expiresAt) {
+        this.id = id;
+        this.user = user;
+        ExpiresAt = expiresAt;
+    }
+}
